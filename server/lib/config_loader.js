05f6f11fbdc1c653526ab15439d523ce071df8f5
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2018-01-27 15:35:08 +0300

    Lets begin developing of BDEV project

--- server/lib/config_loader.js
+++
@@ -1,9 +0,0 @@
-
-global.config = loadConfig();
-
-function loadConfig() {
-    if (process.env.NODE_ENV === 'local' || process.env.NODE_ENV === 'localhost') {
-        return require('../config/local')
-    }
-    return require('../config/config')
-}
